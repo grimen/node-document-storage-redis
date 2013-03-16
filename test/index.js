e5ccb7dd918602632adce3f64a2b14bbb2c67bad
Jonas Grimfelt <grimen@gmail.com>
2013-03-16 17:26:35 +0100

    Make connection URL protocol map to storage adapter.

--- test/index.js
+++ test/index.js
@@ -8,6 +8,8 @@ var Storage = require('node-document-storage');
 module.exports = Storage.Spec('Redis', {
   module: require('..'),
   engine: require('redis'),
+  id: 'redis',
+  protocol: 'redis',
   db: 'default-test',
   default_url: 'redis://localhost:6379/default-test',
   authorized_url: 'redis://redistogo:57c5cf5c220a6b376cf3740297e0f69f@slimehead.redistogo.com:9501/test',
@@ -54,4 +56,3 @@ module.exports = Storage.Spec('Redis', {
     }
   }
 });
-
